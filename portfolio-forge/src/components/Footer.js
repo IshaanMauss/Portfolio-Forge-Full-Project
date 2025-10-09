382d0a25b5b4cdab970ba1cb21ef884904c30a56
IshaanMauss <ishaanchaturvedi444@gmail.com>
2025-10-09 22:17:13 +0530

    :linkedin work polished

--- portfolio-forge/src/components/Footer.js
+++ portfolio-forge/src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
         <svg width="24" height="24" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M20 80V20H35V50H65V20H80V80H65V50H35V80H20Z" fill="#64ffda"/>
         </svg>
-        <span>Forged by ITV</span>
+        <span>Forged by WRAP</span>
       </div>
     </footer>
   );
